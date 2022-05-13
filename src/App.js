3a9fddacf8e5481a7cc9b9449c35145ddbc17706
saisuvanth <saisuvanth@gmail.com>
2022-05-13 16:55:50 +0530

    hehe

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
